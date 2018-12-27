043f1ff44ce22b23d70b37f6958c4b3cf86080e6
Harsh Gandhi <harshnar@buffalo.edu>
2018-12-27 13:57:45 -0500

    implemented quick and insertion sort

--- SortingAlgorithms.py
+++ SortingAlgorithms.py
@@ -19,6 +19,42 @@ def selectionsort(nums):
         if index is not i:
             nums[i],nums[index] = nums[index], nums[i]
     return nums
+
+def insertionsort(nums):
+    for i in range(len(nums)):
+        j = i
+        while j>0 and nums[j-1]>nums[j]:
+            nums[j],nums[j-1] = nums[j-1], nums[j]
+            j-=1
+    return nums
+
+
+def quickPartition(nums,low,high):
+    pivot = (low + high)//2
+    nums[high],nums[pivot] = nums[pivot],nums[high]
+    
+    i = low
+    for j in range(low,high):
+        if nums[j]<=nums[high]:
+            nums[i],nums[j] = nums[j] , nums[i]
+            i+=1
+    
+    nums[i],nums[high] = nums[high],nums[i]
+    return i
+
+def quicksort(nums,low,high):
+    if low>=high:
+        return
+    pivot  = quickPartition(nums,low,high)
+    quicksort(nums, low, pivot-1)
+    quicksort(nums, pivot+1, high)
+    
+
+
+
+
+
+
 if __name__ =="__main__":
     a = [1,5,4,3,55,32,232,122]
     
@@ -27,3 +63,11 @@ if __name__ =="__main__":
     
     print("\nSelection sort")
     print(selectionsort(a))
+    
+    print("\nInsertion Sort")
+    print(insertionsort(a))
+    
+    print("\n Quick Sort")
+    print(a)
+    quicksort(a, 0, len(a)-1)
+    print(a)
